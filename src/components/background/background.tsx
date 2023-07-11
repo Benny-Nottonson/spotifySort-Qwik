b28debb31c1298245fd77f82ae2eb35ef4a6e423
benny <benny.nottonson01@student.csulb.edu>
2023-07-11 08:58:27 -0700

    size fix

--- src/components/background/background.tsx
+++ src/components/background/background.tsx
@@ -14,11 +14,39 @@ export default component$<{}>(() => {
   return (
     <div>
       <AnimatedCircle
-        size={60}
-        position={[30, 0]}
+        size={13}
+        position={[25, 20]}
         direction={1}
+        velocity={8}
+        distance={30}
+      />
+      <AnimatedCircle
+        size={9}
+        position={[26, 65]}
+        direction={-1}
         velocity={5}
-        distance={100}
+        distance={30}
+      />
+      <AnimatedCircle
+        size={6}
+        position={[22, 65]}
+        direction={-1}
+        velocity={3}
+        distance={50}
+      />
+      <AnimatedCircle
+        size={4}
+        position={[40, 65]}
+        direction={-1}
+        velocity={3.5}
+        distance={30}
+      />
+      <AnimatedCircle
+        size={5}
+        position={[55, 65]}
+        direction={-1}
+        velocity={3.5}
+        distance={30}
       />
     </div>
   );
