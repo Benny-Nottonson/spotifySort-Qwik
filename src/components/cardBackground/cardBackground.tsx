782efe7dea668976202b989b4eed85213b25962d
Benny Nottonson <bennynottonson@gmail.com>
2023-07-12 22:03:26 -0700

    removed tailwind - need to do carousel and logic

--- src/components/cardBackground/cardBackground.tsx
+++ src/components/cardBackground/cardBackground.tsx
@@ -18,24 +18,6 @@ export default component$(() => {
           <rect width="100vw" height="100vh" filter="url(#noiseFilter)" />
         </svg>
       </div>
-      <div class="radialContainer">
-        <svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
-          <defs>
-            <radialGradient id="radialGradient" cx="70%" cy="70%" r="100%">
-              <stop offset="0%" stopColor="#1ED760" stopOpacity="1" />
-              <stop offset="40%" stopColor="#62CAFF" stopOpacity="0.4" />
-              <stop offset="100%" stopColor="#2279CA" stopOpacity="0.2" />
-            </radialGradient>
-          </defs>
-          <rect
-            x="0"
-            y="0"
-            width="100"
-            height="100"
-            fill="url(#radialGradient)"
-          />
-        </svg>
-      </div>
     </div>
   );
 });
