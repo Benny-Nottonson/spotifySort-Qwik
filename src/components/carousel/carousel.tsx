ef9b2c6f5f13c6efa12dab3214e107fd625887c4
Benny Nottonson <bennynottonson@gmail.com>
2023-07-12 22:13:21 -0700

    ts

--- src/components/carousel/carousel.tsx
+++ src/components/carousel/carousel.tsx
@@ -1,12 +1,15 @@
-import { component$, useVisibleTask$, useSignal } from "@builder.io/qwik";
+import { component$, useVisibleTask$, useSignal, useStylesScoped$, $ } from "@builder.io/qwik";
 import Playlist from "~/components/playlist/playlist";
+import css from "./carousel.css?inline";
 
 interface CarouselProps {
   token: string;
 }
 
 export default component$(({ token }: CarouselProps) => {
+  useStylesScoped$(css);
   const playlists = useSignal<string[]>([]);
+  const currentIndex = useSignal<number>(0);
 
   useVisibleTask$(() => {
     fetch("https://api.spotify.com/v1/me/playlists", {
@@ -23,11 +26,30 @@ export default component$(({ token }: CarouselProps) => {
       });
   });
 
+  const prevItem = $(
+    () => {
+       currentIndex.value = (currentIndex.value - 1 + playlists.value.length) % playlists.value.length;
+     }
+  );
+
+  const nextItem = $(
+    () => {
+       currentIndex.value = (currentIndex.value + 1) % playlists.value.length;
+     }
+  );
+
   return (
-    <>
-      {playlists.value.map((playlistId: string) => (
-        <Playlist key={playlistId} token={token} id={playlistId}></Playlist>
+    <div class="carousel">
+      <div class="carousel-arrow left" onClick$={prevItem}>{`<`}</div>
+      {playlists.value.map((playlistId: string, index: number) => (
+        <div
+          key={playlistId}
+          class={`carousel-item ${index === currentIndex.value ? "center" : index === currentIndex.value - 1 ? "left" : index === currentIndex.value - 1 ? "right" : "far"}`}
+        >
+          <Playlist token={token} id={playlistId} />
+        </div>
       ))}
-    </>
+      <div class="carousel-arrow right" onClick$={nextItem}>{`>`}</div>
+    </div>
   );
 });
