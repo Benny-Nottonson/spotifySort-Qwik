35843c887d5b7b8aca2bb3b2a39a28cfc13cd46f
benny <benny.nottonson01@student.csulb.edu>
2023-07-14 10:21:51 -0700

    anylstics

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -1,5 +1,7 @@
 import { component$, Slot } from "@builder.io/qwik";
 import type { RequestHandler } from "@builder.io/qwik-city";
+import { inject } from '@vercel/analytics';
+
 
 export const onGet: RequestHandler = async ({ cacheControl }) => {
   // Control caching for this request for best performance and to reduce hosting costs:
@@ -13,5 +15,6 @@ export const onGet: RequestHandler = async ({ cacheControl }) => {
 };
 
 export default component$(() => {
+  inject();
   return <Slot />;
 });
